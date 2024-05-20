a4f1c38471f627334f00a876e5bf5b13e33a34ce
Rubén <rubenvarelafigueroa@gmail.com>
2024-05-20 16:22:11 +0200

    Página de NexoGamer funcional y mergeada

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Inicio from './Cabecera/Cabecera';
 import JuegosNombre from './JuegosNombre/JuegosNombre';
+import Login from './Login_Register/Login';
+import Register from './Login_Register/Register';
 import Carrito from './Carrito/Carrito';
 import InfoJuegos from './InfoJuegos/InfoJuegos';
 
@@ -13,6 +15,8 @@ function App() {
           <Route index element={<Inicio></Inicio>}></Route>
           <Route path="/inicio" element={<Inicio></Inicio>}></Route>
           <Route path="juegosNombre/:juego" element={<JuegosNombre></JuegosNombre>}></Route>
+          <Route path='login' element={<Login></Login>}></Route>
+          <Route path='register' element={<Register></Register>}></Route>
           <Route path='carrito' element={<Carrito></Carrito>}></Route>
           <Route path='juego/:id' element={<InfoJuegos></InfoJuegos>}></Route>
         </Routes>
